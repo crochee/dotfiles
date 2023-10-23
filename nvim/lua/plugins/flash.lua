5e30587aee5c3873242538f4977d3d1ba5dad22d
crochee <13522570308@139.com>
2023-10-24 00:08:03 +0800

    [Refactor] flash快捷键变更

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -3,58 +3,41 @@ local M = {
   event = 'VeryLazy',
   keys = {
     {
-      't',
-      mode = { 'n', 'x', 'o' },
-      function()
-        require('flash').jump()
-      end,
-      desc = 'Flash Jump',
+      "s",
+      mode = { "n", "x", "o" },
+      function() require("flash").jump() end,
+      desc = "Flash"
     },
     {
-      '<leader>ft',
-      mode = { 'n' },
-      function()
-        require('flash').treesitter()
-      end,
-      desc = 'Flash Treesitter Search',
+      "S",
+      mode = { "n", "x", "o" },
+      function() require("flash").treesitter() end,
+      desc = "Flash Treesitter"
     },
     {
-      '<leader>ts',
-      mode = { 'n' },
-      function()
-        require('flash').treesitter_search()
-      end,
-      desc = 'Flash Treesitter Search',
+      "r",
+      mode = "o",
+      function() require("flash").remote() end,
+      desc = "Remote Flash"
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function() require("flash").treesitter_search() end,
+      desc = "Treesitter Search"
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function() require("flash").toggle() end,
+      desc =
+      "Toggle Flash Search"
     },
   },
 }
 
 function M.config()
-  require('flash').setup({
-    modes = {
-      search = {
-        enabled = false,
-      },
-      char = {
-        enabled = false,
-        keys = { 'f', 'F', 't', 'T' },
-      },
-    },
-  })
-  -- to use this, make sure to set `opts.modes.char.enabled = false`
-  local Config = require('flash.config')
-  local Char = require('flash.plugins.char')
-  for _, motion in ipairs({ 'f', 'F' }) do
-    vim.keymap.set({ 'n', 'x', 'o' }, motion, function()
-      require('flash').jump(Config.get({
-        mode = 'char',
-        search = {
-          mode = Char.mode(motion),
-          max_length = 1,
-        },
-      }, Char.motions[motion]))
-    end)
-  end
+  require('flash').setup()
 end
 
 return M
