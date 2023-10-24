2d942b391fbcd0cc3d73050224622054e2ebee8d
crochee <13522570308@139.com>
2023-10-24 09:27:21 +0800

    [Refactor] 去除ft的增强

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -15,29 +15,22 @@ local M = {
       desc = "Flash Treesitter"
     },
     {
-      "r",
-      mode = "o",
-      function() require("flash").remote() end,
-      desc = "Remote Flash"
-    },
-    {
-      "R",
-      mode = { "o", "x" },
+      "<leader>fr",
+      mode = { "n", "o", "x" },
       function() require("flash").treesitter_search() end,
       desc = "Treesitter Search"
-    },
-    {
-      "<c-s>",
-      mode = { "c" },
-      function() require("flash").toggle() end,
-      desc =
-      "Toggle Flash Search"
     },
   },
 }
 
 function M.config()
-  require('flash').setup()
+  require('flash').setup({
+    modes = {
+      char = {
+        enabled = false,
+      }
+    }
+  })
 end
 
 return M
