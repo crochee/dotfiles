bf8759c70ece4be52db69855d3d034d636900c20
crochee <13522570308@139.com>
2024-06-28 10:02:11 +0800

    refactor: remove gitsign

--- nvim/lua/configs/keymaps.lua
+++ nvim/lua/configs/keymaps.lua
@@ -201,32 +201,7 @@ map("n", "<leader>dh", ":lua require'dapui'.eval()<CR>", opts)
 map("n", "<leader>dgt", ":lua require('dap-go').debug_test()<CR>", opts)
 map("n", "<leader>dgl", ":lua require('dap-go').debug_last_test()<CR>", opts)
 
-
---------------------gitsigns ----------------------------------
-pluginKeys.mapgit = function(mapbuf)
-  -- Navigation
-  mapbuf('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-  mapbuf('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
-
-  -- Actions
-  mapbuf('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-  mapbuf('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-  mapbuf('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-  mapbuf('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-  mapbuf('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
-  mapbuf('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
-  mapbuf('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
-  mapbuf('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
-  mapbuf('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-  mapbuf('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
-  mapbuf('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
-  mapbuf('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-  mapbuf('n', '<leader>td', '<cmd>gitsigns toggle_deleted<cr>')
-
-  -- Text object
-  mapbuf('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  mapbuf('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-end
+-----------------------------git --------------------------------
 -- git history for select
 map({ 'n', 'v' }, '<leader>gl', "<Cmd>lua require'git-log'.check_log()<CR>", dopts("git log history for select"))
 
