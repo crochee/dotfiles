ec63898faa41689d9b84b65862b331449c2a9b67
crochee <13522570308@139.com>
2024-07-25 16:57:02 +0800

    refactor: 删除重命名iferr，mason下载后自动加载

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -2,22 +2,22 @@
 -- Add iferr declaration
 -- That's Lua of vimscript implementation of:
 -- github.com/koron/iferr
-function IFERR()
-	local boff = vim.fn.wordcount().cursor_bytes
-	local cmd = ("iferr -pos " .. boff)
-	local data = vim.fn.systemlist(cmd, vim.fn.bufnr("%"))
-
-	if vim.v.shell_error ~= 0 then
-		vim.notify("command " .. cmd .. " exited with code " .. vim.v.shell_error)
-		return
-	end
-
-	local pos = vim.fn.getcurpos()[2]
-	vim.fn.append(pos, data)
-	vim.cmd([[silent normal! j=2j]])
-	vim.fn.setpos(".", pos)
-end
-
-local map = vim.keymap.set
-
-map({ "i", "n" }, "<C-i>", "<CMD>lua IFERR()<CR>", { noremap = true, silent = true })
+-- function IFERR()
+-- 	local boff = vim.fn.wordcount().cursor_bytes
+-- 	local cmd = ("iferr -pos " .. boff)
+-- 	local data = vim.fn.systemlist(cmd, vim.fn.bufnr("%"))
+--
+-- 	if vim.v.shell_error ~= 0 then
+-- 		vim.notify("command " .. cmd .. " exited with code " .. vim.v.shell_error)
+-- 		return
+-- 	end
+--
+-- 	local pos = vim.fn.getcurpos()[2]
+-- 	vim.fn.append(pos, data)
+-- 	vim.cmd([[silent normal! j=2j]])
+-- 	vim.fn.setpos(".", pos)
+-- end
+--
+-- local map = vim.keymap.set
+--
+-- map({ "i", "n" }, "<C-i>", "<CMD>lua IFERR()<CR>", { noremap = true, silent = true })
