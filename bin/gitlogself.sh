2926f1c4ceada4b47bb58f935e6e2b3e99384754
crochee <13522570308@139.com>
2024-08-02 16:42:26 +0800

    refactor: date with $2

--- bin/gitlogself.sh
+++ bin/gitlogself.sh
@@ -5,41 +5,40 @@ set -e
 
 PWD=$(pwd)
 
-beforedate=$(date -d "$1" +'%Y-%m-%d')
-afterdate=$(date -d "$1"'-7 day' +'%Y-%m-%d')
-echo "afterdate: $afterdate  beforedate: $beforedate"
-
 PROJECT=~/workspace/project/dcs
 
 function git_checkout() {
-	cur_branch=$(git branch --show-current)
-	if [[ "$1" != "$cur_branch" ]]; then
-		has_branch=$(git branch --list "$1")
-		if [[ -z "$has_branch" ]]; then
-			has_remote_branch=$(git branch -r | grep "$1")
-			if [[ -z "$has_remote_branch" ]]; then
-				return 1
-			fi
+    cur_branch=$(git branch --show-current)
+    if [[ "$1" != "$cur_branch" ]]; then
+        has_branch=$(git branch --list "$1")
+        if [[ -z "$has_branch" ]]; then
+            has_remote_branch=$(git branch -r | grep "$1")
+            if [[ -z "$has_remote_branch" ]]; then
+                return 1
+            fi
 
-			git fetch
-			git checkout -b "$1" "origin/$1"
-			return
-		fi
+            git fetch
+            git checkout -b "$1" "origin/$1"
+            return
+        fi
 
-		if ! git checkout "$1"; then
-			return 1
-		fi
-	fi
-	git pull
+        if ! git checkout "$1"; then
+            return 1
+        fi
+    fi
+    git pull
 }
 
 dirs=$(find "$PROJECT" -maxdepth 1 -type d ! -wholename "$PROJECT")
 for i in $dirs; do
-	echo "$i"
-	cd "$i"
-	if git_checkout "$2"; then
-		git log --after="$afterdate" --before="$beforedate" --author="crochee" | cat
-	fi
+    echo "$i"
+    cd "$i"
+    if git_checkout "$1" && [[ -n "$2" ]]; then
+        beforedate=$(date -d "$2" +'%Y-%m-%d')
+        afterdate=$(date -d "$2"'-7 day' +'%Y-%m-%d')
+        echo "afterdate: $afterdate  beforedate: $beforedate"
+        git log --after="$afterdate" --before="$beforedate" --author="crochee" | cat
+    fi
 done
 
 cd "$PWD"
