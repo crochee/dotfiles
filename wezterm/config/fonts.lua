6f91b8567e02b3bf3bbbac38fb296a941f5f676b
crochee <13522570308@139.com>
2024-08-16 17:36:54 +0800

    refactor: rewrite term map

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -1,28 +1,10 @@
-local wezterm = require('wezterm')
-local platform = require('utils.platform')
-
-
-local function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return 'Dark'
-end
-
-local function theme_for_appearance(appearance)
-  if appearance:find 'Dark' then
-    return 'Molokai'
-  else
-    return 'Catppuccin Macchiato (Gogh)'
-  end
-end
+local wezterm = require("wezterm")
+local platform = require("utils.platform")
 
 return {
-  -- color scheme
-  color_scheme = 'Catppuccin Macchiato (Gogh)',
-
-  font_size = platform().is_mac and 20 or 18,
-  line_height = 1.2,
-  font = wezterm.font('FantasqueSansM Nerd Font Mono', { weight = 'Bold', italic = true }),
-
+	-- color scheme
+	color_scheme = "Catppuccin Macchiato (Gogh)",
+	font_size = platform().is_mac and 20 or 18,
+	line_height = 1.2,
+	font = wezterm.font("FantasqueSansM Nerd Font Mono", { weight = "Bold", italic = true }),
 }
--- wezterm/config/launch.lua
+++ wezterm/config/launch.lua
@@ -1,35 +1,30 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
-local platform = require('utils.platform')()
+local platform = require("utils.platform")()
 
 local options = {
-  default_prog = {},
-  launch_menu = {},
+	default_prog = {},
+	launch_menu = {},
 }
 
 if platform.is_win then
-  if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    options.default_prog = { 'wsl', '~' }
-  else
-    options.default_prog = { 'pwsh' }
-  end
-  options.launch_menu = {
-    { label = 'wsl',                args = { 'wsl', '~' } },
-    { label = 'PowerShell Core',    args = { 'pwsh' } },
-    { label = 'PowerShell Desktop', args = { 'powershell' } },
-    { label = 'Command Prompt',     args = { 'cmd' } },
-    { label = 'admin powershell',   args = { 'powershell', '-command', 'Start-Process powershell -Verb RunAs' } },
-  }
-elseif platform.is_mac then
-  options.default_prog = { 'bash', '-l' }
-  options.launch_menu = {
-    { label = 'Bash', args = { 'bash', '-l' } },
-  }
-elseif platform.is_linux then
-  options.default_prog = { 'bash', '-l' }
-  options.launch_menu = {
-    { label = 'Bash', args = { 'bash', '-l' } },
-  }
+	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+		options.default_prog = { "wsl", "~" }
+	else
+		options.default_prog = { "pwsh" }
+	end
+	options.launch_menu = {
+		{ label = "wsl", args = { "wsl", "~" } },
+		{ label = "PowerShell Core", args = { "pwsh" } },
+		{ label = "PowerShell Desktop", args = { "powershell" } },
+		{ label = "Command Prompt", args = { "cmd" } },
+		{ label = "admin powershell", args = { "powershell", "-command", "Start-Process powershell -Verb RunAs" } },
+	}
+elseif platform.is_mac or platform.is_linux then
+	options.default_prog = { "bash", "-l" }
+	options.launch_menu = {
+		{ label = "Bash", args = { "bash", "-l" } },
+	}
 end
 
 return options
