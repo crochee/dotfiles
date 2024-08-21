f9bb9fb18853be0825664a2ee9d8a269d5e9e7bc
crochee <13522570308@139.com>
2024-08-21 09:28:43 +0800

    refactor: 变更prom本地搭建流程

--- k8scnf/prometheus.sh
+++ k8scnf/prometheus.sh
@@ -1,1 +1,1 @@
-docker run --name prometheus -itd -p 9090:9090  -v /home/crochee/workspace/configs/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+docker run --name prometheus -itd -p 9090:9090  -v /home/crochee/.config/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
