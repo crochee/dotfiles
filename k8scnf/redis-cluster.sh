24a0cf8c8d0843ef47676e2bec5386005732522f
crochee <13522570308@139.com>
2024-09-26 11:41:27 +0800

    refactor: fix image tag

--- k8scnf/redis-cluster.sh
+++ k8scnf/redis-cluster.sh
@@ -1,1 +1,1 @@
-docker run -e "IP=0.0.0.0" -e "STANDALONE=true" -e "SENTINEL=true" -p 7000-7005:7000-7005  --name redis-cluster --restart always  --net host  -d grokzen/redis-cluster
+sudo docker run -e "IP=0.0.0.0" -e "STANDALONE=true" -e "SENTINEL=true" -p 7000-7005:7000-7005 --name redis-cluster --restart always -d grokzen/redis-cluster:6.0.16
