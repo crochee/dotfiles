da6af667a7aa8d7a6a3d8f780fddf5d796a4a0a4
crochee <13522570308@139.com>
2024-09-27 11:33:28 +0800

    refactor(wezterm): adjust font size 22

--- config/wezterm/config/fonts.lua
+++ config/wezterm/config/fonts.lua
@@ -4,7 +4,7 @@ local platform = require("utils.platform")
 return {
 	-- color scheme
 	color_scheme = "Catppuccin Macchiato (Gogh)",
-	font_size = platform().is_mac and 20 or 18,
+	font_size = platform().is_mac and 24 or 22,
 	line_height = 1.2,
 	font = wezterm.font_with_fallback({
 		{ family = "FantasqueSansM Nerd Font Mono", weight = "Bold", italic = true },
