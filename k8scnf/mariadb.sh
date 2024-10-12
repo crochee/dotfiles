f9ae0b4f3f2bf24c69f88c59ab821f0fa819eba2
crochee <13522570308@139.com>
2024-10-12 14:29:40 +0800

    refactor: todo使用默认配置

--- k8scnf/mariadb.sh
+++ k8scnf/mariadb.sh
@@ -1,1 +1,1 @@
-docker run -itd --name mariadb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234567 --restart=always -v /home/crochee/store/mariadb/:/var/lib/mysql mariadb
+sudo docker run -itd --name mariadb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234567 --restart=always -v /home/crochee/store/mariadb/:/var/lib/mysql mariadb
