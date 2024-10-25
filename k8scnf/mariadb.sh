a129d76994a72c68aa49247bf648b39617cdbbd9
crochee <13522570308@139.com>
2024-10-25 15:41:16 +0800

    refactor: 去掉sudo执行命令

--- k8scnf/mariadb.sh
+++ k8scnf/mariadb.sh
@@ -1,1 +1,1 @@
-sudo docker run -itd --name mariadb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234567 --restart=always -v /home/crochee/store/mariadb/:/var/lib/mysql mariadb
+docker run -itd --name mariadb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1234567 --restart=always -v /home/crochee/store/mariadb/:/var/lib/mysql mariadb
--- k8scnf/redis-cluster.sh
+++ k8scnf/redis-cluster.sh
@@ -1,1 +1,1 @@
-sudo docker run -e "IP=0.0.0.0" -e "STANDALONE=true" -e "SENTINEL=true" -p 7000-7005:7000-7005 --name redis-cluster --restart always -d grokzen/redis-cluster:6.0.16
+docker run -e "IP=0.0.0.0" -e "STANDALONE=true" -e "SENTINEL=true" -p 7000-7005:7000-7005 --name redis-cluster --restart always -d grokzen/redis-cluster:6.0.16
