79784f92416261eb99afd1c1fb3c78c52f3dad62
crochee <13522570308@139.com>
2024-11-11 09:33:14 +0800

    refactor: add py wk

--- config/nvim/lua/lsp/pyright.lua
+++ config/nvim/lua/lsp/pyright.lua
@@ -1,23 +1,31 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pyright
 -- 先下载anguage server
 -- npm i -g pyright
-local util = require "lspconfig/util"
+local util = require("lspconfig/util")
 local opts = {
-  cmd = { "pyright-langserver", "--stdio" },
-  filetypes = { "python" },
-  root_dir = util.root_pattern(".git"),
-  single_file_support = true,
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        diagnosticMode = "workspace",
-        useLibraryCodeForTypes = true
-      }
-    }
-  },
-  capabilities = require('lsp.utils').capabilities,
-  on_attach = require('lsp.utils').on_attach
+	cmd = { "pyright-langserver", "--stdio" },
+	filetypes = { "python" },
+	root_dir = util.root_pattern(
+		"pyproject.toml",
+		"setup.py",
+		"setup.cfg",
+		"requirements.txt",
+		"Pipfile",
+		"manage.py",
+		".git"
+	),
+	single_file_support = true,
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "workspace",
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
+	capabilities = require("lsp.utils").capabilities,
+	on_attach = require("lsp.utils").on_attach,
 }
 
 return opts
