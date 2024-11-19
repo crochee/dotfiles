66a05a0d15e107e2677b9369066a3a38e0c93d50
crochee <13522570308@139.com>
2024-11-19 17:30:43 +0800

    refactor: rw dapui event watch

--- config/nvim/lua/daps/dapui.lua
+++ config/nvim/lua/daps/dapui.lua
@@ -71,12 +71,15 @@ dapui.setup({
 		max_type_length = nil, -- Can be integer or nil.
 	},
 }) -- use default
-dap.listeners.after.event_initialized["dapui_config"] = function()
+dap.listeners.before.attach.dapui_config = function()
 	dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
 	dapui.close()
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
+dap.listeners.before.event_exited.dapui_config = function()
 	dapui.close()
 end
