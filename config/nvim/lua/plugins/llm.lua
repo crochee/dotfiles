b0ef79ab49fdf55c3c72ee14af3cd8ad7d7aebe9
crochee <13522570308@139.com>
2025-02-28 10:09:22 +0800

    feat: config/nvim/lua/plugins/llm.lua, added flexible window options and commit action mapping

--- config/nvim/lua/plugins/llm.lua
+++ config/nvim/lua/plugins/llm.lua
@@ -99,6 +99,22 @@ Based on this format, generate appropriate commit messages. Respond with message
 					opts = {
 						enter_flexible_window = true,
 						apply_visual_selection = false,
+						win_opts = {
+							relative = "editor",
+							position = "50%",
+						},
+						accept = {
+							mapping = {
+								mode = "n",
+								keys = "<cr>",
+							},
+							action = function()
+								local contents = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+								vim.api.nvim_command(
+									string.format('!git commit -m "%s"', table.concat(contents, '" -m "'))
+								)
+							end,
+						},
 					},
 				},
 				DocString = {
