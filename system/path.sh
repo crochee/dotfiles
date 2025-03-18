3f646266203b4b4d545ad26cc89eb26b872ef46a
crochee <13522570308@139.com>
2025-03-18 17:27:44 +0800

    chore: update system/path.sh, removed nvim-linux64 from PATH

--- system/path.sh
+++ system/path.sh
@@ -11,7 +11,5 @@ prepend-path "$HOME/.local/share/nvim/mason/bin"
 # set PATH so it includes user's private bin if it exists
 prepend-path "$HOME/.local/bin"
 
-prepend-path "$HOME/.local/bin/nvim-linux64/bin"
-
 prepend-path "$CARGO_HOME/bin"
 
