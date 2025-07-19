29986adcd70c427f33cff2fa4be5c6977e6466f1
crochee <13522570308@139.com>
2025-07-19 23:00:48 +0800

    feat: add kulala

---
+++ config/nvim/lua/plugins/kulala.lua
@@ -0,0 +1,11 @@
+return {
+	"mistweaverco/kulala.nvim",
+	ft = { "http", "rest" },
+	opts = {
+		ui = {
+			display_mode = "float",
+		},
+		global_keymaps = true,
+        global_keymaps_prefix = "<leader>k",
+	},
+}
