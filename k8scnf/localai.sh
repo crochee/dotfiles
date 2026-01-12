b435836784f684d31b154554a38968d59d3131f2
crochee <licf137@chinaunicom.cn>
2026-01-12 14:41:58 +0800

    refactor: local ai with shimmy

--- k8scnf/localai.sh
+++
@@ -1,1 +0,0 @@
-docker run -itd --name local-ai -e MODELS_PATH=/models -e CACHE_DIR=/tmp/local-ai -v ~/.models:/models -v ~/.cache/local-ai:/tmp/local-ai -p 8080:8080 --restart=always localai/localai:latest
